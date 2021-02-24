f6415fda5cc5debb8ce9e5a7fd59c4609ca1a8e9
Alexandru Cone <alexandru.cone.ac@gmail.com>
2021-02-24 22:18:34 +0100

    Initial Commit

---
+++ Volantiere/VolantiereApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolantiereApp.swift
+//  Volantiere
+//
+//  Created by Alexandru Cone on 24/02/21.
+//
+
+import SwiftUI
+
+@main
+struct VolantiereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
