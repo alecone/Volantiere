0ef9a0321fa8deae2e387e3559b36c597f90cc45
Alexandru Cone <alexandru.cone.ac@gmail.com>
2021-03-20 19:30:33 +0100

    Added LICU view

---
+++ Volantiere/Views/LICUView.swift
@@ -0,0 +1,20 @@
+//
+//  LICUView.swift
+//  Volantiere
+//
+//  Created by Alexandru Cone on 20/03/21.
+//
+
+import SwiftUI
+
+struct LICUView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LICUView_Previews: PreviewProvider {
+    static var previews: some View {
+        LICUView()
+    }
+}
